04a6cd441752261603c3089e79d43989c15a3819
Paulo <paulorenato.cpb@gmail.com>
2019-09-17 00:07:16 -0300

    operations table

--- src/components/operation/Operations.vue
+++ src/components/operation/Operations.vue
@@ -1,15 +1,50 @@
 <template>
   <v-container fluid>
-    <v-layout v-if="user" justify-center>
-      <v-data-table
-        :loading="loading"
-        loading-text="Carregando operações"
-        :headers="headers"
-        :items="user.operations"
-        :sort-by="['time']"
-        :sort-desc="[true]"
-        class="elevation-1"
-      ></v-data-table>
+    <v-layout v-if="user">
+      <v-flex xs12>
+        <v-data-table
+          :loading="loading"
+          loading-text="Carregando operações"
+          :headers="headers"
+          :items="user.operations"
+          item-key="id"
+          :sort-by="['time']"
+          :sort-desc="[true]"
+          show-expand
+          :expanded.sync="expanded"
+          class="elevation-1"
+        >
+          <template v-slot:expanded-item="{ item }">
+            <td :colspan="headers.length + 1">
+              <v-layout row v-if="item.type === 'trade'">
+                <v-flex xs12>Moeda de taxa: {{item.commissionAsset}}</v-flex>
+                <v-flex xs12>Valor da taxa: {{item.commission}}</v-flex>
+              </v-layout>
+              <v-layout row v-if="item.type === 'whitdraw'">
+                <v-flex xs12>Endereço: {{item.address}}</v-flex>
+                <v-flex xs12>ID da transação: {{item.txId}}</v-flex>
+                <v-flex xs12>Taxa da transação: {{item.transactionFee}}</v-flex>
+              </v-layout>
+              <v-layout row v-if="item.type === 'deposit'">
+                <v-flex xs12>Endereço: {{item.address}}</v-flex>
+                <v-flex xs12>ID da transação: {{item.txId}}</v-flex>
+              </v-layout>
+            </td>
+          </template>
+          <template v-slot:item.time="{ item }">{{formatDate(item.time)}}</template>
+          <template
+            v-slot:item.isBuyer="{ item }"
+          >{{item.isBuyer === undefined ? "" : (item.isBuyer === true ? 'Compra' : 'Venda')}}</template>
+          <template
+            v-slot:item.type="{ item }"
+          >{{item.type === "trade" ? "Trade" : (item.type === "whitdraw" ? 'Saque' : 'Depósito')}}</template>
+          <template v-slot:item.qty="{ item }">{{Number(item.qty).toFixed(8)}}</template>
+          <template v-slot:item.price="{ item }">{{item.price ? Number(item.price).toFixed(8) : ""}}</template>
+          <template
+            v-slot:item.quoteQty="{ item }"
+          >{{item.quoteQty ? Number(item.quoteQty).toFixed(8) : ""}}</template>
+        </v-data-table>
+      </v-flex>
     </v-layout>
   </v-container>
 </template>
@@ -20,22 +55,26 @@ import Date from "@/mixins/Date";
 export default {
   mixins: [Date],
   data: () => ({
+    expanded: [],
     headers: [
-      { text: "Operação", value: "type" },
-      { text: "Exchange", value: "exchangeName" },
+      { text: "Data", align: "right", value: "time", sortable: false },
+      { text: "Operação", align: "right", value: "type", sortable: false },
       {
-        text: "Par",
-        align: "left",
-        value: "symbol"
+        text: "Exchange",
+        align: "right",
+        value: "exchangeName",
+        sortable: false
       },
-      { text: "Tipo", value: "operationType" },
-      { text: "Quantidade", value: "qty" },
-      { text: "Preço", value: "price" },
-      { text: "Valor", value: "quoteQty" },
-      { text: "Taxa (Moeda)", value: "commissionAsset" },
-      { text: "Taxa", value: "commission" },
-      { text: "Data", value: "time" },
-      { text: "Data Formatada", value: "timef" }
+      {
+        text: "Moeda",
+        align: "right",
+        value: "symbol",
+        sortable: false
+      },
+      { text: "Tipo", align: "right", value: "isBuyer", sortable: false },
+      { text: "Quantidade", align: "right", value: "qty", sortable: false },
+      { text: "Preço (un)", align: "right", value: "price", sortable: false },
+      { text: "Valor", align: "right", value: "quoteQty", sortable: false },
     ]
   }),
   computed: {
